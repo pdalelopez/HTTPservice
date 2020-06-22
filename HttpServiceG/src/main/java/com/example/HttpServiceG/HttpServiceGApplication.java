744b7a5b2e5718ab1f1c2b0d52fc7a5de65c5477
pdalelopez <pdalelopez@users.noreply.github.com>
2020-06-22 10:58:02 +0200

    Project created

---
+++ HttpServiceG/src/main/java/com/example/HttpServiceG/HttpServiceGApplication.java
@@ -0,0 +1,13 @@
+package com.example.HttpServiceG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpServiceGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpServiceGApplication.class, args);
+	}
+
+}
---
+++ HttpServiceG/src/main/java/com/example/HttpServiceG/domain/Employee.java
@@ -0,0 +1,4 @@
+package com.example.HttpServiceG.domain;
+
+public class Employee {
+}
